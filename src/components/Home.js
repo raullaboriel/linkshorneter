415c4ff570b823daa1e8faaa1f1dcd70386f7ae2
Raul <raul.laboriel@gmail>
2022-04-03 15:22:48 -0600

    styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -43,10 +43,11 @@ const Home = (props) => {
         }
 
         if (validURL(link)) {
-            if(link.startsWith('https://lilink.herokuapp.com') || link.startsWith('lilink.herokuapp.com')){
+            if (link.startsWith('https://lilink.herokuapp.com') || link.startsWith('lilink.herokuapp.com')) {
                 addToast('danger', 'Ese link ya está recortado.');
                 return;
             }
+
             let originalLink = link;
 
             if (originalLink.substring(0, 7) !== 'http://' && originalLink.substring(0, 8) !== 'https://') {
@@ -80,12 +81,18 @@ const Home = (props) => {
         <div className="App">
             <div className="container mt-4 py-4">
                 <div className="d-flex flex-row mb-4">
-                    <form className="col-12" onSubmit={(e) => onShortLink(e)}>
+                    <form className="col" onSubmit={(e) => onShortLink(e)}>
                         <div className="row justify-content-center flex-nowrap">
-                            <input style={{ 'backgroundColor': 'rgb(58,59,61)', 'fontWeight': 'bold' }} autoComplete="off" type="text" name='link' autoCorrect="off" autoCapitalize="none" onChange={(e) => handleInputChange(e)} value={link} className="form-control text-white flex-fill mr-2 p-4 border-0" placeholder="Ingresa tu enlace" />
+                            <input style={{ 'backgroundColor': 'rgb(58,59,61)', 'fontWeight': 'bold' }}
+                                autoComplete="off" type="text" name='link' autoCorrect="off" autoCapitalize="none"
+                                onChange={(e) => handleInputChange(e)} value={link}
+                                className="form-control p-4 text-white flex-fill mr-2 border-0"
+                                placeholder="Ingresa tu enlace" />
                             {
                                 showShortenURL ?
-                                    <button type="submit" style={{ 'backgroundColor': 'rgb(79,70,229)', 'fontSize': '18px' }} className="btn border-0 col-3 font-weight-bold btn-primary">
+                                    <button type="submit"
+                                        style={{ 'backgroundColor': 'rgb(79,70,229)', 'fontSize': '18px' }}
+                                        className="btn border-0 col-lg-3 col-md-3 col-sm-3 col-xl-3x col-4 font-weight-bold btn-primary">
                                         Recortar
                                     </button>
                                     :
@@ -93,7 +100,7 @@ const Home = (props) => {
                                         type='button'
                                         onClick={() => { navigator.clipboard.writeText(link); setIsLinkCopied(true) }}
                                         style={{ 'backgroundColor': 'rgb(79,70,229)', 'fontSize': '18px' }}
-                                        className={`btn col-3 font-weight-bold btn-primary ${isLinkCopied ? 'border-succes border-lg' : 'border-0'} ${isLinkCopied && 'border-success'}`}
+                                        className={`btn pb-3 pt-3 border-0 col-lg-3 col-md-3 col-sm-3 col-xl col-4 font-weight-bold btn-primary ${isLinkCopied ? 'border-succes border-lg' : 'border-0'} ${isLinkCopied && 'border-success'}`}
                                     >
                                         {isLinkCopied ? '¡Copiado!' : 'Copiar'}
                                     </button>

1f891fe71ccd482931b863d6f4ff591708dffb8f
Raul <raul.laboriel@gmail>
2022-09-30 17:31:29 -0600

    updates

--- src/origin.js
+++ src/origin.js
@@ -1,6 +1,6 @@
 const origin = () => {
   if (process.env.NODE_ENV === "production") {
-    return "https://link-shortener-api-production.up.railway.app/";
+    return "https://link-shortener-api-production.up.railway.app";
   }
   return "http://localhost:5000";
 };
